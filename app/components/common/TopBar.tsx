031cd101d8a271a709febc0cf5f1cbd666ecaace
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2023-10-28 22:14:01 +0900

    updated

---
+++ app/components/common/TopBar.tsx
@@ -0,0 +1,19 @@
+import ImgBlock from "@/app/components/common/ImgBlock";
+import "@/styles/Common.css";
+import Link from "next/link";
+import {ReactNode} from "react";
+interface ElementType {text:string,class:string,href:string}
+export default function TopBar({children}:{children:Array<ElementType>}){
+	const menus:Array<ReactNode> = [];
+	children.forEach((element:ElementType,index:number)=>{
+		menus.push(<Link key={index} className={element.class} href={element.href}>{element.text}</Link>);
+	});
+	return(
+	<div className={"pl-4 pr-16 flex items-center h-16 w-screen justify-between border-b-gray-200 border-2 fixed bg-white z-10"}>
+		<ImgBlock src={"/assets/svg/logo.png"} width={"7rem"} height={"4rem"}/>
+		<div className={"flex gap-8 items-center"}>
+			{menus}
+		</div>
+	</div>
+	);
+}
