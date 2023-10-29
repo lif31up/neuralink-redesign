c0cc81fea912104612dfedefc834dbd4295cd0f4
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2023-10-29 11:12:33 +0900

    updated

---
+++ app/components/common/SVGBlock.tsx
@@ -0,0 +1,5 @@
+export default function SVGBLock({src,width,height}:{src:string,width:string,height:string}){
+	return(
+	<object data={src} width={width} height={height}/>
+	);
+}
--- app/components/common/TopBar.tsx
+++ app/components/common/TopBar.tsx
@@ -2,6 +2,7 @@ import ImgBlock from "@/app/components/common/ImgBlock";
 import "@/styles/Common.css";
 import Link from "next/link";
 import {ReactNode} from "react";
+import SVGBLock from "@/app/components/common/SVGBlock";
 interface ElementType {text:string,class:string,href:string}
 export default function TopBar({children}:{children:Array<ElementType>}){
 	const menus:Array<ReactNode> = [];
@@ -10,7 +11,7 @@ export default function TopBar({children}:{children:Array<ElementType>}){
 	});
 	return(
 	<div className={"pl-4 pr-16 flex items-center h-16 w-screen justify-between border-b-gray-200 border-2 fixed bg-white z-10"}>
-		<ImgBlock src={"/assets/svg/logo.png"} width={"7rem"} height={"4rem"}/>
+		<SVGBLock src={"/assets/svg/logo.svg"} width={(14*7).toString()} height={(14*4).toString()}/>
 		<div className={"flex gap-8 items-center"}>
 			{menus}
 		</div>
