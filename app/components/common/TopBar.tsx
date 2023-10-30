736ede8c9c62cc5f7a1118a3f510e7f3ce973255
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2023-10-30 13:08:25 +0900

    updated

--- app/components/common/TopBar.tsx
+++ app/components/common/TopBar.tsx
@@ -2,6 +2,7 @@ import "@/styles/Common.css";
 import Link from "next/link";
 import {ReactNode} from "react";
 import SVGBLock from "@/app/components/common/SVGBlock";
+import "@/styles/TopBar.css";
 interface ElementType {text:string,class:string,href:string}
 export default function TopBar({children}:{children:Array<ElementType>}){
 	const menus:Array<ReactNode> = [];
@@ -9,7 +10,7 @@ export default function TopBar({children}:{children:Array<ElementType>}){
 		menus.push(<Link key={index} className={element.class} href={element.href}>{element.text}</Link>);
 	});
 	return(
-	<div className={"pl-4 pr-16 flex items-center h-16 w-screen justify-between border-b-gray-200 border-2 fixed bg-white z-10"}>
+	<div className={"custom-top-bar"}>
 		<SVGBLock src={"/assets/svg/logo.svg"} width={(14*7).toString()} height={(14*4).toString()}/>
 		<div className={"flex gap-8 items-center"}>
 			{menus}
--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,14 @@ const Footer = lazy(()=>import("@/app/components/common/Footer"));
 import Tag from "@/app/components/common/Tag";
 import Section from "@/app/components/common/Section";
 import Link from "next/link";
-import Image from "next/image";
 import ImgBlock from "@/app/components/common/ImgBlock";
 export default function Home() {
   return (<>
     <title>Neuralink</title>
     <div className={"block min-h-screen bg-white"}>
       <TopBar>{[
-        {text:"Career",href:"",class:"p-0 m-0"},
-        {text:"Contact",href:"",class:"p-0 m-0"},
+        {text:"Career",href:"https://neuralink.com/careers/",class:"p-0 m-0 hover:border-b-black hover:border-b-2 hover:-mb-0.5"},
+        {text:"Contact",href:"https://neuralink.com/blog/",class:"p-0 m-0 hover:border-b-black hover:border-b-2 hover:-mb-0.5"},
         {text:"Patient Registry",href:"/patient_registry",class:"custom-outline p-1 rounded-2xl px-4 hover:bg-black hover:text-white"}
       ]}</TopBar>
       <div className={"h-16"}/>
@@ -41,8 +40,19 @@ export default function Home() {
         <Link className={"absolute right-1 bottom-1"} href={"career/"}>See More about Implant Process</Link>
       </Section>
       <Section>
-        <Tag h1={"A Seamless BCI Experience"} p={"To restore independence and improve lives, we’ve built a brain-computer interface (BCI) experience that enables fast and reliable computer control and prioritizes ease of use."}/>
-        <Tag h1={"Patient Registry"} p={"If you're interested in learning whether you may qualify for current and/or future Neuralink clinical trials, consider joining our Patient Registry."}/>
+        <h1 className={"text-2xl"}>Implant</h1>
+        <Tag h1={"Implant"} p={"Our brain-computer interface is fully implantable, cosmetically invisible, and designed to let you control a computer or mobile device anywhere you go."}/>
+        <Tag h1={"Biocompatible Enclosure"} p={"The N1 Implant is hermetically sealed in a biocompatible enclosure that withstands physiological conditions several times harsher than those in the human body."}/>
+        <Tag h1={"Battery"} p={"The N1 Implant is powered by a small battery charged wirelessly from the outside via a compact, inductive charger that enables easy use from anywhere."}/>
+        <Tag h1={"Chips and Electronics"} p={"Advanced, custom, low-power chips and electronics process neural signals, transmitting them wirelessly to the Neuralink Application, which decodes the data stream into actions and intents."}/>
+        <Tag h1={"Threads"} p={"The N1 Implant records neural activity through 1024 electrodes distributed across 64 threads. These highly-flexible, ultra-thin threads are key to minimize damage during implantation and beyond."}/>
+      </Section>
+      <Section>
+        <h1 className={"text-2xl"}>Surgical Robot</h1>
+        <Tag h1={"Surgical Robot"} p={"The threads of our implant are so fine that they can't be inserted by the human hand. Our surgical robot has been designed to reliably and efficiently insert these threads exactly where they need to be."}/>
+        <Tag h1={"Base Structure"} p={"The base structure and motion stage provide the structural platform for the robot head and the primary 3 axis linear motion used to position the robot head and needle."}/>
+        <Tag h1={"Robot Head"} p={"The robot head contains the optics and sensors of 5 camera systems and the optics for an optical coherence tomography (OCT) system."}/>
+        <Tag h1={"Needle"} p={"The needle, which is thinner than a human hair, grasps, inserts, and releases threads."}/>
       </Section>
       <React.Suspense>
         <Footer/>
