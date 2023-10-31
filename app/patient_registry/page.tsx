ea6eeffba69db43b1825d089e408ab579308e672
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2023-11-01 08:08:11 +0900

    Update page.tsx

--- app/patient_registry/page.tsx
+++ app/patient_registry/page.tsx
@@ -14,14 +14,14 @@ export default function patientRegistry(){
 				{text:"Patient Registry",href:"/patient_registry",class:"custom-outline p-1 rounded-2xl px-4 hover:bg-black hover:text-white"}
 			]}</TopBar>
 			<div className={"h-16"}/>
-			<Section>
+			<Section className={""}>
 				<h1>{"Patient Registry"}</h1>
 				<p className={"text-2xl"}>{"If you're interested in learning whether you may qualify for current and/or future Neuralink clinical trials, consider joining our Patient Registry."}</p>
 			</Section>
-			<Section>
+			<Section className={""}>
 				<ExpandableTagTable/>
 			</Section>
-			<Section>
+			<Section className={""}>
 				<h1>{"Apply for Consideration"}</h1>
 				<p className={"text-2xl"}>{"If you're interested in helping us develop general-purpose brain-computer interfaces with the potential to help restore independence to individuals with disabilities, consider joining the Patient Registry. If you have any questions or concerns, feel free to reach out to patientregistry@neuralink.com"}</p>
 			</Section>
